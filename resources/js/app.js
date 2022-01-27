0bc0e652d0679e578ebd67e0c186c9bac53a73aa
tastytim <tastytimgm@gmail.com>
2022-01-27 17:25:16 +0100

    created route.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.Vue = require('axios');
 
 /**
  * The following block of code may be used to automatically register your
@@ -26,7 +27,11 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+ 
+import router from '../js/router.js';
+
 
 const app = new Vue({
+    router,
     el: '#app',
 });
