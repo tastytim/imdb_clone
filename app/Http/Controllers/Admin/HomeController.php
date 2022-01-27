e62b81a8493a32caa3df0cc527e23be914849d72
andre-cyb <andrea.vg.malfatti@gmail.com>
2022-01-27 11:54:45 +0100

    fixati bug HomeController e rotte admin/guest

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class HomeContoller extends Controller
+class HomeController extends Controller
 {
     function index() {
         return view('admin.home');
