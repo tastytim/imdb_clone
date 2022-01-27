3a59369d3e8188e49cba2e4b790c106525eb05c7
Bravehearts19 <leo18111998@gmail.com>
2022-01-27 17:30:18 +0100

    file for vue components created

---
+++ resources/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <!-- <transition> -->
+        <router-view></router-view>
+        <!-- </transition> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    components: {  },
+    data() {
+        return {
+            helloMsg: "Hello world",
+        };
+    },
+};
+</script>
